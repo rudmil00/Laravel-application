e798539023d84882cc0b4989f6174b2b77adaf1a
rudmil00 <milanrudanzr@gmail.com>
2023-01-21 00:12:57 +0100

    Kreiranje kontrolera
    Signed-off-by: rudmil00 <milanrudanzr@gmail.com>

---
+++ muzicka_skola/app/Http/Controllers/TerminController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Termin;
+use Illuminate\Http\Request;
+
+class TerminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Termin  $termin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Termin $termin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Termin  $termin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Termin $termin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Termin  $termin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Termin $termin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Termin  $termin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Termin $termin)
+    {
+        //
+    }
+}
